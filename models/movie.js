7b9d1b25a820c12ea125a09ff9d3b3e7d1258a55
Rustam Malikov <mrj@lowcost-mebel.ru>
2023-05-31 22:27:29 +0300

    feat: настроить схему movie

--- models/movie.js
+++ models/movie.js
@@ -1,41 +1,76 @@
 const mongoose = require('mongoose');
+const incorrectLinkMessage = require('../constants');
 
 const movieSchema = new mongoose.Schema({
   country: {
-
+    type: String,
+    required: true,
   },
   director: {
-
+    type: String,
+    required: true,
   },
   duration: {
-
+    type: Number,
+    required: true,
   },
   year: {
-
+    type: String,
+    required: true,
   },
   description: {
-
+    type: String,
+    required: true,
   },
   image: {
-
+    type: String,
+    required: true,
+    validate: {
+      validator: (v) => {
+        const urlRegex = /^(http:\/\/www\.|https:\/\/www\.|http:\/\/|https:\/\/)?[a-z0-9]+([-.]{1}[a-z0-9]+)*\.[a-z]{2,5}(:[0-9]{1,5})?(\/.*)?$/i;
+        return urlRegex.test(v);
+      },
+      message: incorrectLinkMessage,
+    },
   },
   trailerLink: {
-
+    type: String,
+    required: true,
+    validate: {
+      validator: (v) => {
+        const urlRegex = /^(http:\/\/www\.|https:\/\/www\.|http:\/\/|https:\/\/)?[a-z0-9]+([-.]{1}[a-z0-9]+)*\.[a-z]{2,5}(:[0-9]{1,5})?(\/.*)?$/i;
+        return urlRegex.test(v);
+      },
+      message: incorrectLinkMessage,
+    },
   },
   thumbnail: {
-
+    type: String,
+    required: true,
+    validate: {
+      validator: (v) => {
+        const urlRegex = /^(http:\/\/www\.|https:\/\/www\.|http:\/\/|https:\/\/)?[a-z0-9]+([-.]{1}[a-z0-9]+)*\.[a-z]{2,5}(:[0-9]{1,5})?(\/.*)?$/i;
+        return urlRegex.test(v);
+      },
+      message: incorrectLinkMessage,
+    },
   },
   owner: {
-
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'user',
+    required: true,
   },
   movieId: {
-
+    type: Number,
+    required: true,
   },
   nameRU: {
-
+    type: String,
+    required: true,
   },
   nameEN: {
-
+    type: String,
+    required: true,
   },
 });
 
