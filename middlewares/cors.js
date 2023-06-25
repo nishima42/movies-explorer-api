96aa99c246736482c3d58edd7e8f1cce25798e66
Rustam Malikov <mrj@lowcost-mebel.ru>
2023-06-25 21:01:44 +0300

    ref: изменить cors

--- middlewares/cors.js
+++ middlewares/cors.js
@@ -5,7 +5,10 @@ function cors(req, res, next) {
   res.setHeader('Access-Control-Allow-Credentials', 'true');
 
   if (req.method === 'OPTIONS') {
-    res.status(200).end();
+    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PATCH, PUT, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    res.setHeader('Access-Control-Allow-Credentials', 'true');
   } else {
     next();
   }
